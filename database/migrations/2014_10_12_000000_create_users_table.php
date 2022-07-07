dbec035c660b760d31a3c24ef51f5796c93d3eed
unknown <shankhantanoli1@gmail.com>
2022-07-07 13:15:22 +0400

    New Update

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -21,15 +21,26 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->enum('role', ['support', 'customer']);
             $table->rememberToken();
             $table->timestamps();
         });
 
-        for($support = 1; $support < 6; $support++){
+        for ($support = 1; $support < 4; $support++) {
             User::create([
-                'name' => 'Agent'.$support,
-                'email' => 'agent'.$support.'@gmail.com',
+                'name' => 'Agent' . $support,
+                'email' => 'agent' . $support . '@gmail.com',
                 'password' => Hash::make('password'),
+                'role' => 'support',
+            ]);
+        }
+
+        for ($customer = 1; $customer < 4; $customer++) {
+            User::create([
+                'name' => 'Customer' . $customer,
+                'email' => 'customer' . $customer . '@gmail.com',
+                'password' => Hash::make('password'),
+                'role' => 'customer',
             ]);
         }
     }
